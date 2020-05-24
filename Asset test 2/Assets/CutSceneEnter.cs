75472d3710610f6dc3021f23425c30c2933013a0
Samuel J <johnstonsamuel@inaburrastudents.nsw.edu.au>
2020-05-25 07:46:14 +1000

    Update 4

---
+++ Asset test 2/Assets/CutSceneEnter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneEnter : MonoBehaviour
+{
+    public GameObject thePlayer;
+    public GameObject cutSceneCam;
+    public GameObject theEagle;
+    public BoxCollider2D cameraHitbox;
+
+    void Start()
+    {
+        cutSceneCam.SetActive(false);
+        theEagle.SetActive(false);
+    }
+
+    void OnCollisionEnter2D(Collision2D CollisionInfo)
+    {
+        cameraHitbox.enabled = false;
+        cutSceneCam.SetActive(true);
+        thePlayer.SetActive(false);
+        theEagle.SetActive(true);
+        StartCoroutine(FinishCut());
+    }
+
+    IEnumerator FinishCut()
+    {
+        yield return new WaitForSeconds(5);
+        thePlayer.SetActive(true);
+        cutSceneCam.SetActive(false);
+    }
+}
